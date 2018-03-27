8f846fa4b14ab1a6b2587ad753d039d1e1dd8778
Jon Huang <biohfj@gmail.com>
2018-03-27 23:49:58 +0800

    init modal component

---
+++ web/src/components/Modal/index.js
@@ -0,0 +1,15 @@
+import FlexDialog from './FlexDialog';
+import ModalBackground from './ModalBackground';
+import ModalContainer from './ModalContainer';
+import ModalDialog from './ModalDialog';
+import ModalPortal from './ModalPortal';
+import UnstyledFlexDialog from './UnstyledFlexDialog';
+
+export {
+  FlexDialog,
+  ModalBackground,
+  ModalContainer,
+  ModalDialog,
+  ModalPortal,
+  UnstyledFlexDialog,
+};
