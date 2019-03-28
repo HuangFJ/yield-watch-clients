28cb49c60bca11da204aca34686b68eb25f8838e
Jon Huang <biohfj@gmail.com>
2019-03-28 16:51:36 +0800

    change ajax url

--- web/src/constants.js
+++ web/src/constants.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://watch.yield.newfish.com/api";
+export const API_BASE_URL = "https://watch.yield.newfish.com:4443/api";
 export const DB_NAME = 'yield';
 export const DB_VERSION = 1;
